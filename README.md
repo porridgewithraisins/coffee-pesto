19ee3b005f53007468a9998114921fd8842f9fb6
Santhanam <me@santhanam.dev>
2023-10-15 16:20:37 +0530

    move custom search out to hook

---
+++ README.md
@@ -0,0 +1,119 @@
+# Coffeepesto
+
+A clipboard manager for linux (X11 only) that
+
+- is tiny, but supports a whole lot of features.
+- supports all kinds of data - rich text, images, files in your file manager - you name it.
+- has an impressive search feature (search by window, date, day, time)
+  - you can even add custom data to the search index!
+- has a configurable and potentially infinite history, regardless of the type of data
+- can blacklist windows easily
+- is modular and file-based so you can easily take parts of it and fit it into your existing setup (rofi, dmenu, anything) and is easy to script with
+
+It only touches the clipboard (the one where you ctrl+c and right click copy), not the other X11 stuff.
+
+## Demo
+
+TODO: insert demo video
+
+## How to get
+
+```bash
+git clone https://github.com/porridgewithraisins/coffee-pesto
+
+gcc `pkg-config --libs --cflags gtk+-3.0` -Wno-incompatible-pointer-types choose.c -o chooselisting
+
+sudo cp clipdaemon chooselisting restorelisting /usr/local/bin
+```
+
+## How to use
+
+- Run `clipdaemon` in the background by adding it to your autostart.
+- Bind `chooselisting | xargs restorelisting` to a keyboard shortcut (such as super+v) and you will get a popup at your mouse for you to choose something from your history.
+- In this popup, you can right click an item to delete it, and double click an item to put it on the clipboard.
+- You can use the search bar to filter items. Out of the box, you can search by
+  - the app you copied from, for all types of data
+  - the date/day/time you copied it from (in `/usr/bin/date` format), for all types of data
+  - (fuzzy) any part of the text itself, for textual data.
+
+### Blacklist windows
+
+To blacklist windows, create a file (if it does not exist) at `~/.coffeepesto/blacklist`. In this file, write a _single_ line with a regex (extended grep) matching the names of windows you want to blacklist. For one, you can use this to prevent passwords copied from your password manager from entering your clipboard history, where it would be stored plaintext for anyone to read. You have to avoid partial matches yourself. It is case insensitive.
+
+### To temporarily pause clipboard history
+
+You just have to create a file `~/.coffeepesto/pause`. Remove that file to resume.
+
+### Cross-device sync
+
+Just sync the `~/.coffeepesto` directory with any existing file sync tool - rsync, dropbox, or have this directory on a shared server.
+
+### Change base directory
+
+You can change the default of `~/.coffeepesto` by setting the `COFFEE_PESTO_DIR` environment variable.
+
+### If copying certain things doesn't work
+
+I have ignored certain targets i.e types of data in order to reduce clutter. Most applications work just fine. Suppose you copy something and it doesn't show up in your history. Check if _all_ the available targets for the copied data (Copy it, and run `getcp TARGETS` to check) are being ignored in the `ignored_targets` variable in the `clipdaemon` script.
+
+## Hacking
+
+### Custom search information
+
+Callback style. You can create any executable at `~/.coffeepesto/custom-index`. It will be passed the directory containing all files related to the just-copied clipboard item as the 1st argument. See [here](#data-structures-used) to understand how to read the data structure.
+
+As a random idea, you can make your clipboard entries searchable by whether you were at work or at home. You can query the current wifi network and use that to conditionally append `work` or `home` to the file.
+
+This also means you can make this executable a sort of post-copy hook, if you want to do that, for... some reason.
+
+### How it works
+
+- `daemon` takes whatever is copied and stores it in ~/.coffeepesto/h folder. It also stores metadata to be used in search queries.
+- `chooselisting` reads that folder and shows the listings. It puts to `stdout` the listing number you chose. It offers fuzzy search capabilities on the data and metadata stored by `daemon`.
+- `restore` takes a listing number as argument, and puts the data at that listing number on the clipboard.
+
+### How the x11 clipboard works
+
+It is convoluted. There is no separate buffer called the "clipboard". Instead, the window you copy from, and the window you copy to, do _message passing_ to send the data you copy.
+
+There is also such a thing as "targets", which is usually the type of data. For example, when you copy an image on google docs, it doesn't put the image on your clipboard, it instead puts an `<img src='some url'>` with the target as `text/html`, and when you ctrl+v, it requests the target `text/html` and then displays it as an image. If you copy a file in your file manager, it doesnt copy the whole file to your clipboard, obviously. It just copies the file path, sets the target as `file-list` or something along those lines. Then when you paste the file, it requests data as that target.
+
+### Data structures used
+
+```
+  $ tree ~/.coffeepesto
+  ~/.coffeepesto
+  └── h
+      ├── 1
+      │   ├── text\html
+      │   │   ├── data
+      │   │   └── index
+      │   └── text\plain
+      │       ├── data
+      │       └── index
+      ├── 2
+      │   ├── image\png
+      │   │   ├── data
+      │   │   └── index
+      │   └── text\html
+      │       ├── data
+      │       └── index
+      └── 3
+          └── image\png
+              ├── data
+              └── index
+```
+
+Inside the `h` subfolder, each numeral-named folder corresponds to a different item ("listing").
+
+Inside each such listing `$i`, there is one folder for each target. For each target, there is a data file and an index file. The data file has, well, the data itself. The index file contains metadata for search purposes. Additionally, for text-based data, the index file also contains a copy of the data. Metadata is duplicated for each target.
+
+Since we are taking target names which are untrusted input and interpreting it as a filesystem entry, we have to reject a few target names. We only allow `[a-zA-Z-/]` in target names, and convert `/` to `\` before writing to the filesystem. This should cover all normal targets.
+
+### Additional files
+
+- A `~/.coffeepesto/blacklist` file contains a single line extended grep regex which will be used to identify windows that are blacklisted. Copies from windows identified as such will not be saved into history.
+
+- A `~/.coffeepesto/ourpaste` file is created by the `restorelisting` tool so that the item you restored doesn't go into the history again and is ignored by the daemon.
+
+- While a file exists at `~/.coffeepesto/pause`, items will not be saved to clipboard history. It is used for temporarily pausing clipboard history.
---
+++ choose.c
@@ -0,0 +1,256 @@
+#include <gtk/gtk.h>
+
+#define WINDOW_HEIGHT 400
+#define WINDOW_WIDTH 350
+
+#define RENDER_TYPE_FAIL 0
+#define RENDER_TYPE_IMAGE 1
+#define RENDER_TYPE_TEXT 2
+#define RENDER_TYPE_UNKNOWN 3
+
+const gchar *historydir = NULL;
+
+GtkWidget *render_listing(const gchar *listing);
+gboolean onclick(GtkEventBox *eventbox, GdkEventButton *event, gpointer unused) {
+    const GtkListBoxRow *row = gtk_widget_get_parent(eventbox);
+    const GtkListBox *listbox = gtk_widget_get_parent(row);
+    const char *listing = g_object_get_data(row, "listing");
+
+    switch (event->button) {
+        case 1:
+            printf("%s\n", listing);
+            gtk_main_quit();
+            return TRUE;
+        case 2:
+            gchar *listingdir = g_build_filename(historydir, listing, NULL);
+            gchar *negative = g_strdup_printf("%ld", -strtol(listing, NULL, 10));
+            gchar *negative_listingdir = g_build_filename(historydir, negative, NULL);
+            if (rename(listingdir, negative_listingdir) != 0) {
+                fprintf(stderr, "Failed to pin/unpin item\n");
+                return TRUE;
+            }
+
+            GtkListBox *listbox = gtk_widget_get_parent(row);
+            gtk_widget_destroy(row);
+            GtkListBoxRow *updated_row = render_listing(negative);
+            gtk_list_box_insert(listbox, updated_row, -1);
+            gtk_list_box_invalidate_sort(listbox);
+            gtk_widget_show_all(listbox);
+            return TRUE;
+        case 3:
+            listingdir = g_build_filename(historydir, listing, NULL);
+            if (!g_file_trash(g_file_new_for_path(listingdir), NULL, NULL)) {
+                system(g_strconcat("/bin/rm -r ", listingdir, NULL));
+            }
+            gtk_widget_destroy(row);
+            return TRUE;
+    }
+}
+
+int what_to_render(const gchar *listing, gchar **chosen_dir) {
+    gchar *listing_dir = g_build_filename(historydir, listing, NULL);
+    GDir *dir = g_dir_open(listing_dir, 0, NULL);
+    if (!dir) {
+        fprintf(stderr, "Failed to open listing %s\n", listing);
+        return RENDER_TYPE_FAIL;
+    }
+
+    gchar *first_text_entry = NULL, *first_image_entry = NULL;
+    const gchar *entry = NULL;
+    while ((entry = g_dir_read_name(dir))) {
+        if (!entry) {
+            fprintf(stderr, "Error reading a variant of listing %s\n", listing);
+            continue;
+        }
+        if (g_str_equal(entry, "text\\plain")) {
+            *chosen_dir = g_build_filename(listing_dir, entry, NULL);
+            return RENDER_TYPE_TEXT;
+        }
+        if (!first_text_entry && g_str_has_prefix(entry, "text\\")) {
+            first_text_entry = g_build_filename(listing_dir, entry, NULL);
+        }
+        if (!first_image_entry && g_str_has_prefix(entry, "image\\")) {
+            first_image_entry = g_build_filename(listing_dir, entry, NULL);
+        }
+    }
+
+    if (first_image_entry) {
+        *chosen_dir = first_image_entry;
+        return RENDER_TYPE_IMAGE;
+    }
+    if (first_text_entry) {
+        *chosen_dir = first_text_entry;
+        return RENDER_TYPE_TEXT;
+    }
+
+    *chosen_dir = NULL;
+    return RENDER_TYPE_UNKNOWN;
+}
+
+GtkWidget *render_listing(const gchar *listing) {
+    GtkWidget *widget = NULL;
+    gchar *chosen_dir = NULL;
+    switch (what_to_render(listing, &chosen_dir)) {
+        case RENDER_TYPE_TEXT: {
+            gchar *data_file = g_build_filename(chosen_dir, "data", NULL);
+            gchar *text = NULL;
+            if (!g_file_get_contents(data_file, &text, NULL, NULL)) {
+                fprintf(stderr, "Failed to read text data from %s\n", data_file);
+                return NULL;
+            }
+            widget = gtk_label_new(g_utf8_substring(text, 0, MIN(strlen(text), 100)));  // dont ask
+            g_free(text);
+            gtk_label_set_ellipsize(widget, PANGO_ELLIPSIZE_END);
+            break;
+        }
+        case RENDER_TYPE_IMAGE: {
+            gchar *data_file = g_build_filename(chosen_dir, "data", NULL);
+            GdkPixbuf *buffer = gdk_pixbuf_new_from_file_at_size(data_file, 400, 200, NULL);
+            if (!buffer) {
+                fprintf(stderr, "Failed to read image data from %s\n", data_file);
+                return NULL;
+            }
+            widget = gtk_image_new_from_pixbuf(buffer);
+            break;
+        }
+        case RENDER_TYPE_UNKNOWN:
+            widget = gtk_label_new("(blob)");
+            break;
+        case RENDER_TYPE_FAIL:
+        default:
+            return NULL;
+    }
+
+    gchar *index_file = g_build_filename(chosen_dir, "index", NULL);
+    gchar *index_data = NULL;
+    if (!g_file_get_contents(index_file, &index_data, NULL, NULL)) {
+        fprintf(stderr, "Failed to read index data from %s\n", index_file);
+        return NULL;
+    }
+    GtkOverlay *overlay = gtk_overlay_new();
+    gtk_container_add(overlay, widget);
+    if (listing[0] == '-') {
+        GtkImage *pinned_icon = gtk_image_new_from_icon_name("pin", GTK_ICON_SIZE_DND);
+        gtk_widget_set_halign(pinned_icon, GTK_ALIGN_END);
+        gtk_widget_set_valign(pinned_icon, GTK_ALIGN_START);
+        gtk_overlay_add_overlay(overlay, pinned_icon);
+    }
+
+    GtkEventBox *eventbox = gtk_event_box_new();
+    g_signal_connect(eventbox, "button-release-event", onclick, NULL);
+    gtk_container_add(eventbox, overlay);
+
+    GtkListBoxRow *row = gtk_list_box_row_new();
+    gtk_container_add(row, eventbox);
+    g_object_set_data(row, "listing", g_strdup(listing));
+    g_object_set_data(row, "index", index_data);
+    gtk_widget_set_tooltip_text(row, "Left click to copy, middle click to pin, right click to delete");
+
+    return row;
+}
+
+gboolean onescape(GtkWidget *widget, GdkEventKey *event, gpointer unused) {
+    if (event->keyval == GDK_KEY_Escape) {
+        gtk_main_quit();
+        return TRUE;
+    }
+    return FALSE;
+}
+
+int sorter(GtkListBoxRow *one, GtkListBoxRow *two, gpointer unused) {
+    return strtoul(g_object_get_data(one, "listing"), NULL, 10) < strtoul(g_object_get_data(two, "listing"), NULL, 10);
+}
+
+int clamp(gint min, gint val, gint max) {
+    if (val > max) return max;
+    if (val < min) return min;
+    return val;
+}
+
+gboolean triggerlistupdate(GtkSearchEntry *searchbox, GtkListBox *listbox) {
+    gtk_list_box_invalidate_filter(listbox);
+    return TRUE;
+}
+
+gboolean filterpredicate(GtkListBoxRow *row, GtkSearchEntry *searchbox) {
+    const gchar *searchtext = gtk_entry_get_text(searchbox);
+    const gchar *indexdata = g_object_get_data(row, "index");
+    return g_str_match_string(searchtext, indexdata, TRUE);
+}
+
+int main() {
+    gtk_init(NULL, NULL);
+
+    GtkListBox *listbox = gtk_list_box_new();
+    gtk_list_box_set_activate_on_single_click(listbox, FALSE);
+    gtk_list_box_set_sort_func(listbox, sorter, NULL, NULL);
+
+    const gchar *default_base_dir = g_build_filename(getenv("HOME"), ".coffeepesto", NULL);
+    const gchar *provided_dir = getenv("COFFEE_PESTO_DIR");
+    const gchar *dir_to_use = provided_dir ? provided_dir : default_base_dir;
+    historydir = g_build_filename(dir_to_use, "h", NULL);
+    GDir *fd = g_dir_open(historydir, 0, NULL);
+    if (!fd) {
+        fprintf(stderr, "App directory %s doesn't exist\n", historydir);
+        return 1;
+    }
+
+    const gchar *listing;
+    GtkListBoxRow *row;
+    while ((listing = g_dir_read_name(fd))) {
+        if (!(row = render_listing(listing))) {
+            fprintf(stderr, "Failed to render listing %s. Skipping\n", listing);
+            continue;
+        }
+        gtk_list_box_insert(listbox, row, -1);
+    }
+
+    GtkScrolledWindow *scrollingcontainer = gtk_scrolled_window_new(NULL, NULL);
+    gtk_scrolled_window_set_policy(scrollingcontainer, GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
+    gtk_container_add(scrollingcontainer, listbox);
+
+    GtkSearchEntry *searchbox = gtk_search_entry_new();
+    g_signal_connect(searchbox, "changed", triggerlistupdate, listbox);
+    gtk_list_box_set_filter_func(listbox, filterpredicate, searchbox, NULL);
+
+    GtkHeaderBar *header = gtk_header_bar_new();
+    gtk_header_bar_set_title(header, "CoffeePesto");
+
+    GtkBox *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 2);
+    gtk_box_pack_start(vbox, header, FALSE, FALSE, 0);
+    gtk_box_pack_start(vbox, searchbox, FALSE, FALSE, 0);
+    gtk_box_pack_start(vbox, scrollingcontainer, TRUE, TRUE, 0);
+
+    GtkWindow *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(window, "CoffeePesto");
+    gtk_window_set_decorated(window, FALSE);
+    gtk_window_set_default_size(window, WINDOW_WIDTH, WINDOW_HEIGHT);
+    gtk_window_set_skip_taskbar_hint(window, TRUE);
+    g_signal_connect(window, "focus-out-event", gtk_main_quit, NULL);
+    g_signal_connect(window, "key-press-event", onescape, NULL);
+    gtk_container_add(window, vbox);
+    gtk_widget_show_all(window);
+
+    GdkScreen *screen = gtk_window_get_screen(window);
+    GdkDisplay *display = gtk_widget_get_display(window);
+    GdkMonitor *monitor = gdk_display_get_monitor_at_window(display, gtk_widget_get_window(window));
+    GdkRectangle geometry;
+    gdk_monitor_get_geometry(monitor, &geometry);
+    gint screenwidth = geometry.width;
+    gint screenheight = geometry.height;
+    gint mouseX = 0, mouseY = 0;
+    GdkDevice *device = gdk_seat_get_pointer(gdk_display_get_default_seat(display));
+    gdk_device_get_position(device, &screen, &mouseX, &mouseY);
+    gint desiredX = clamp(100, mouseX, screenwidth - WINDOW_WIDTH - 100);  // 100 is leeway for taskbars n stuff
+    gint desiredY = clamp(100, mouseY, screenheight - WINDOW_HEIGHT - 100);
+    gtk_window_move(window, desiredX, desiredY);
+
+    GtkCssProvider *cssprovider = gtk_css_provider_new();
+    const gchar *css = "row { border-bottom: 0.01em solid @secondary_color; } list { padding: .4em; }";
+    gtk_css_provider_load_from_data(cssprovider, css, -1, NULL);
+    gtk_style_context_add_provider_for_screen(gtk_window_get_screen(window), cssprovider,
+                                              GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+    gtk_main();
+    return 0;
+}
